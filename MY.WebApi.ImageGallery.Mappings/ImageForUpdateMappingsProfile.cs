349be1435a0e416be043c91248e95b83f377bb88
tavakoli.m <tavakoli.m@CL-293>
2022-09-28 15:36:34 +0330

    Add ImageMappingsProfile To Mapping Layer

---
+++ MY.WebApi.ImageGallery.Mappings/ImageForUpdateMappingsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MY.WebApi.ImageGallery.DomainClasses;
+using MY.WebApi.ImageGallery.Models;
+
+namespace MY.WebApi.ImageGallery.Mappings
+{
+    public class ImageForUpdateMappingsProfile:Profile
+    {
+        public ImageForUpdateMappingsProfile()
+        {
+            this.CreateMap<ImageForUpdateModel, Image>()
+                .ForMember(m => m.FileName, options => options.Ignore())
+                .ForMember(m => m.Id, options => options.Ignore())
+                .ForMember(m => m.OwnerId, options => options.Ignore());
+        }
+    }
+}
+
---
+++ MY.WebApi.ImageGallery.Mappings/ImageMappingsProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MY.WebApi.ImageGallery.DomainClasses;
+using MY.WebApi.ImageGallery.Models;
+
+namespace MY.WebApi.ImageGallery.Mappings
+{
+    public class ImageMappingsProfile:Profile
+    {
+        public ImageMappingsProfile()
+        {
+            this.CreateMap<Image, ImageModel>().ReverseMap();
+        }
+    }
+}
+
