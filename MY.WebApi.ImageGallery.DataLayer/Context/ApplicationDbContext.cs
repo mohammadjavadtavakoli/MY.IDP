dc25e475421bb6afc93ebbae2c4c2ddd5dc369c0
tavakoli.m <tavakoli.m@CL-293>
2022-10-02 15:41:17 +0330

    EntityFrameworkCore.Design

--- MY.WebApi.ImageGallery.DataLayer/Context/ApplicationDbContext.cs
+++ MY.WebApi.ImageGallery.DataLayer/Context/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace MY.WebApi.ImageGallery.DataLayer.Context
         {
             
         }
-        public DbSet<Image> Images { get; set; }
+        public virtual DbSet<Image> Images { get; set; }
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
